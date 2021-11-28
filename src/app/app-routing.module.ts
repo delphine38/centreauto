5280793331bac66ba429f7147a5a4b898ac1d5f8
delphine <delphin38@live.fr>
2021-11-28 14:23:04 +0100

    accueil login register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AccueilComponent } from './Accueil/Accueil.component';
 import { RegisterComponent } from './Register/Register.component';
+import { LoginComponent } from './Login/Login.component';
 
 const routes: Routes = [
   {path: 'accueil', component: AccueilComponent},
   {path: 'register', component: RegisterComponent},
+  {path: 'login', component: LoginComponent},
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AccueilComponent } from './accueil/accueil.component';
 import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AccueilComponent,
-    RegisterComponent
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
